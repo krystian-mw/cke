94d22f97ed75f6c49d067e80adc8373ad6f546e7
Krystian <krystian@mwebdev.co.uk>
2021-08-30 18:41:33 +0200

    convert

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,8 +1,6 @@
-/**
- * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
 import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
+// import ClassicEditor from "@ckeditor/ckeditor5-editor-inline/src/inlineeditor.js";
+
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
 import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage.js";
